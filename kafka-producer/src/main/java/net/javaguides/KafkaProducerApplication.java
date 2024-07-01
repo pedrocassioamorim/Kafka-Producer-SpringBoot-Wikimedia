c3f14ee47845e0f87be954c31152ebd2b26e927a
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-07-01 00:36:34 -0300

    Adding Command Line Runner to SpringBootApplication class

--- kafka-producer/src/main/java/net/javaguides/KafkaProducerApplication.java
+++ kafka-producer/src/main/java/net/javaguides/KafkaProducerApplication.java
@@ -1,12 +1,24 @@
 package net.javaguides;
 
+import net.javaguides.kafka.WikimediaChangesProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KafkaProducerApplication {
+public class KafkaProducerApplication implements CommandLineRunner {
 
     public static void main(String[] args){
         SpringApplication.run(KafkaProducerApplication.class);
+    }
+
+    @Autowired
+    private WikimediaChangesProducer wikimediaChangesProducer;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        wikimediaChangesProducer.sendMessage();
     }
 }

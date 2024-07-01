9146e623d4dab852609c1f8e3d6ae377375531de
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-06-30 23:30:13 -0300

    Initiating KAFKA Producer

--- kafka-producer/src/main/java/net/javaguides/KafkaProducerApplication.java
+++ kafka-producer/src/main/java/net/javaguides/KafkaProducerApplication.java
@@ -1,13 +1,12 @@
-package net.javaguides.springboot;
+package net.javaguides;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootProducerApplication {
+public class KafkaProducerApplication {
 
     public static void main(String[] args){
-        SpringApplication.run(SpringBootProducerApplication.class);
+        SpringApplication.run(KafkaProducerApplication.class);
     }
-
 }

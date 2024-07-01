e7d059af49f824862a8d5aa2fce0b2aed7bf9e81
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-06-30 23:13:14 -0300

    Initiating Kafka Producer project

---
+++ src/main/java/net/javaguides/wikimedia_kafka_producer/WikimediaKafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.wikimedia_kafka_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikimediaKafkaProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WikimediaKafkaProducerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/javaguides/wikimedia_kafka_producer/WikimediaKafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.wikimedia_kafka_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikimediaKafkaProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
